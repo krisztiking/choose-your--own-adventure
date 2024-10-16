9665d7aa72cf35977fb81e5a44feff017c30f803
Krisztian Domokos <domokoskrisztian7@gmail.com>
2024-10-16 18:39:21 +0200

    makeing the http server, and the server handler

--- cmd/cyoa-web/main.go
+++ cmd/cyoa-web/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	cyoa "github.com/krisztiking/choose-your--own-adventure"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s. \n", *filename)
@@ -20,10 +22,12 @@ func main() {
 	}
 	defer f.Close()
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", story)
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Staring the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
